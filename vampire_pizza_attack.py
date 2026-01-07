b1847730413c06e54d6d85079a70f55a71d3def6
William LeFevre <wjl30500@gmail.com>
2026-01-06 19:47:38 -0700

    Chapter 2 completed

--- vampire_pizza_attack.py
+++ vampire_pizza_attack.py
@@ -3,13 +3,17 @@ from pygame import *
 
 pygame.init()
 
-WINDOW_WIDTH = 900
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 1100
+WINDOW_HEIGHT = 600
 WINDOW_RES = (WINDOW_WIDTH, WINDOW_HEIGHT)
 
 GAME_WINDOW = display.set_mode(WINDOW_RES)
-
 display.set_caption("Attack of the Vampire Pizzas!")
+
+pizza_img = image.load("game_assets/vampire.png")
+pizza_surf = Surface.convert_alpha(pizza_img)
+VAMPIRE_PIZZA = transform.scale(pizza_surf, (100, 100))
+GAME_WINDOW.blit(VAMPIRE_PIZZA, (900, 400))
 
 game_running = True
 
